09764c8429f239e8f3ec2b7602a18e2167d18f1e
Ayaz Ali <ayaz.ujjan@gmail.com>
2016-11-26 23:29:24 +0100

    little modification

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
   const express = require('express');
   const hbs = require("hbs");
   const fs = require('fs');
+
+  const port = process.env.PORT || 3000;
   var app = express();
 
   hbs.registerPartials(__dirname +"/views/partials")
@@ -64,5 +66,7 @@
     });
   });
 
-  app.listen(3000);
-  console.log("Server is running on port 3000")
+  app.listen(3000, ()=>{
+    console.log(`Server is running on port ${port}`)
+  });
+
