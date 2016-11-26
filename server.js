9ac669a15ba283f714a47d028688ce5a5c4dc814
Ayaz Ali <ayaz.ujjan@gmail.com>
2016-11-26 23:39:59 +0100

    some errors fixed

--- server.js
+++ server.js
@@ -66,7 +66,7 @@
     });
   });
 
-  app.listen(3000, ()=>{
-    console.log(`Server is running on port ${port}`)
+  app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`);
   });
 
